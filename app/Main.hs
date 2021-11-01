765d0d7df32a3ffc96d143fd9d8c45cc1c7989fe
Piyush P Kurur <ppk@cse.iitk.ac.in>
2021-11-01 13:25:09 +0530

    added another unrelated definition.

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,10 @@ module Main where
 
 import GHC.StaticPtr
 
+-- Another static pointer
+optr :: StaticPtr String
+optr = static "hello"
+
 -- | This is the pointer that we print out.
 sptr :: StaticPtr Int
 sptr = static 42
